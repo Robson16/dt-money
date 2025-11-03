cc4ff1a5d9e6042377fe6ee9be72a6a97c32aad5
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-11-03 16:21:47 -0300

    Performance -  Aplicando o useMemo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,29 +1,32 @@
 import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../contexts/TransactionsContext'
+import { useMemo } from 'react'
 
 export function useSummary() {
   const transactions = useContextSelector(TransactionsContext, (context) => {
     return context.transactions
   })
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.amount
-        acc.total += transaction.amount
-      } else {
-        acc.outcome += transaction.amount
-        acc.total -= transaction.amount
-      }
+  const summary = useMemo(() => {
+    transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.amount
+          acc.total += transaction.amount
+        } else {
+          acc.outcome += transaction.amount
+          acc.total -= transaction.amount
+        }
 
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
 
   return summary
 }
